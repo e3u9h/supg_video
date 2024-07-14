525f87f0ad29d1c647ae55b25774bd852e5b004e
e3u9h <dfz2003@126.com>
2024-07-14 19:05:41 +0800

    update

--- supg/experiments/label.py
+++ supg/experiments/label.py
@@ -11,14 +11,17 @@ print('here6')
 import matplotlib.pyplot as plt
 import time
 
-texts = ['a dog', 'a person', 'an elephant', 'a zebra']
-# texts = ['a cat', 'an umbrella', 'a person', 'a chicken']
+#texts = ['a dog', 'a person', 'an elephant', 'a zebra']
+#texts = ['a cat', 'an umbrella', 'a person', 'a chicken']
+#texts = ['a panda', 'a human', 'a bicycle', 'a fox', 'a lion', 'a horse', 'a bird', 'an airplane']
+texts = ['a human', 'a car', 'a dog', 'the sun']
 for text in texts:
+    #source = VideoSource('newout.mp4', text, save=False, oth=0.8)
+    #source = VideoSource('newout.mp4', text, save=True, oth=0.6)
+    source = VideoSource('b_video.mp4', text, save=True, oth=0.6)
     time1 = time.time()
-    source = VideoSource('newout.mp4', text, save=False)
-    # source = VideoSource('b_video.mp4', text)
     length = len(source.proxy_scores)
-    results = source.lookup(range(length), text)
+    results = source.lookup(range(length))
     percentage = sum(results) / length
     print(f'{text}: {percentage}')
     time2 = time.time()
